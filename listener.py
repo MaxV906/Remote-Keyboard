d85795f1f9b982c3962626fd6eadcb462bc3a1ab
AngeLM <vekimax040609@gmail.com>
2022-01-11 19:52:59 +0000

    String support added

--- listener.py
+++ listener.py
@@ -1,4 +1,8 @@
 import socket
+import pynput
+from pynput.keyboard import Key, Controller
+
+keyboard = Controller()
 
 ip = "0.0.0.0"
 port = 1234
@@ -6,8 +10,13 @@ s = socket.socket()
 s.bind((ip, port))
 s.listen()
 conn, addr = s.accept()
-print(f"Connected by: {addr}")
-while True:
-    data = conn.recv(1024)
-    data = data.decode("utf8")
-    print(data)
+with conn:
+    print(f"Connected by: {addr}")
+    while True:
+        data = conn.recv(1024)
+        data = data.decode("utf8")
+        print(data)
+
+        for char in data:
+            keyboard.press(char)
+            keyboard.release(char)
