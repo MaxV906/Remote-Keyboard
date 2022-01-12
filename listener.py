1468c1463441007a8d407862210d4e4d481fa09d
AngeLM <leamax906@gmail.com>
2022-01-12 19:50:06 +0100

    Listener doesn't close after disconnection

--- listener.py
+++ listener.py
@@ -29,89 +29,91 @@ ip = "0.0.0.0"
 port = 1234
 s = socket.socket()
 s.bind((ip, port))
-s.listen()
-conn, addr = s.accept()
-with conn:
-    print(f"Connected by: {addr}")
-    while True:
-        data = conn.recv(1024)
-        data = data.decode("utf8")
+while True:
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print(f"Connected by: {addr}")
+        while True:
+            data = conn.recv(1024)
+            data = data.decode("utf8")
 
-        if data.upper() == "CLOSE;":
-            conn.close()
-            break
-        
-        else:
-            data = data.split(";")
-            data_branches = []
+            if data.upper() == "CLOSE;":
+                conn.close()
+                print (f"{addr} disconnected.")
+                break
+            
+            else:
+                data = data.split(";")
+                data_branches = []
 
-            for part in data:
-                new_branch = part.split()
-                data_branches.append(new_branch)
+                for part in data:
+                    new_branch = part.split()
+                    data_branches.append(new_branch)
 
-            data_branches.pop()
+                data_branches.pop()
 
-            for branch in data_branches:
+                for branch in data_branches:
 
-                if branch[1] == "key.shift":
-                    branch[1] = Key.shift
+                    if branch[1] == "key.shift":
+                        branch[1] = Key.shift
 
-                if branch[1] == "key.alt":
-                    branch[1] = Key.alt
+                    if branch[1] == "key.alt":
+                        branch[1] = Key.alt
 
-                if branch[1] == "key.cmd":
-                    branch[1] = Key.cmd
-                
-                if branch[1] == "key.ctrl":
-                    branch[1] = Key.ctrl
+                    if branch[1] == "key.cmd":
+                        branch[1] = Key.cmd
+                    
+                    if branch[1] == "key.ctrl":
+                        branch[1] = Key.ctrl
 
-                if branch[1] == "key.tab":
-                    branch[1] = Key.tab
+                    if branch[1] == "key.tab":
+                        branch[1] = Key.tab
 
-                if branch[1] == "key.esc":
-                    branch[1] = Key.esc
+                    if branch[1] == "key.esc":
+                        branch[1] = Key.esc
 
-                if branch[1] == "key.enter":
-                    branch[1] = Key.enter
+                    if branch[1] == "key.enter":
+                        branch[1] = Key.enter
 
-                if branch[1] == "key.backspace":
-                    branch[1] = Key.backspace
+                    if branch[1] == "key.backspace":
+                        branch[1] = Key.backspace
 
-                if branch[1] == "key.f1":
-                    branch[1] = Key.f1
+                    if branch[1] == "key.f1":
+                        branch[1] = Key.f1
 
-                if branch[1] == "key.f2":
-                    branch[1] = Key.f2
+                    if branch[1] == "key.f2":
+                        branch[1] = Key.f2
 
-                if branch[1] == "key.f3":
-                    branch[1] = Key.f3
+                    if branch[1] == "key.f3":
+                        branch[1] = Key.f3
 
-                if branch[1] == "key.f4":
-                    branch[1] = Key.f4
+                    if branch[1] == "key.f4":
+                        branch[1] = Key.f4
 
-                if branch[1] == "key.f5":
-                    branch[1] = Key.f5
+                    if branch[1] == "key.f5":
+                        branch[1] = Key.f5
 
-                if branch[1] == "key.f6":
-                    branch[1] = Key.f6
+                    if branch[1] == "key.f6":
+                        branch[1] = Key.f6
 
-                if branch[1] == "key.f7":
-                    branch[1] = Key.f7
+                    if branch[1] == "key.f7":
+                        branch[1] = Key.f7
 
-                if branch[1] == "key.f8":
-                    branch[1] = Key.f8
+                    if branch[1] == "key.f8":
+                        branch[1] = Key.f8
 
-                if branch[1] == "key.f9":
-                    branch[1] = Key.f9
+                    if branch[1] == "key.f9":
+                        branch[1] = Key.f9
 
-                if branch[1] == "key.f10":
-                    branch[1] = Key.f10
+                    if branch[1] == "key.f10":
+                        branch[1] = Key.f10
 
-                if branch[1] == "key.f11":
-                    branch[1] = Key.f11
+                    if branch[1] == "key.f11":
+                        branch[1] = Key.f11
 
-                if branch[1] == "key.f12":
-                    branch[1] = Key.f12
+                    if branch[1] == "key.f12":
+                        branch[1] = Key.f12
 
-                convert(branch)
+                    convert(branch)
 
