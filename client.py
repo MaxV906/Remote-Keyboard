1159c2a1b1e2b811bf785c191e27f7d9ba55a4da
AngeLM <vekimax040609@gmail.com>
2022-01-12 02:46:52 +0000

    Added key combinations and keys

--- client.py
+++ client.py
@@ -5,8 +5,14 @@ port = 1234
 
 def main():
     while True:
-        string = input("String you want to send: ")
-        s.send(string.encode("utf8"))
+        option = input("Choose a method:\n1)Send string\n2)Send keys\nYour answer: ")
+        if option == "1":
+            string = input("\nString you want to send: ")
+            s.send(string.encode("utf8"))
+        else:
+            key_combination = input("\nChoose keys:\n1)alt+f4\n2)ctrl+shift+esc\n3)alt+tab\n4)cmd\n5)tab\n6)up arrow\n7)down arrow\n8)left arrow\n9) right arrow\nYour answer: ")
+            string = "key-combination: " + str(int(key_combination) - 1)
+            s.send(string.encode("utf8"))
 
 while True:
 
--- listener.py
+++ listener.py
@@ -1,8 +1,11 @@
 import socket
 import pynput
+from time import sleep
 from pynput.keyboard import Key, Controller
 
 keyboard = Controller()
+
+key_combos = ["alt+f4", "ctrl+shift+esc", "alt+tab", "cmd", "tab", "up arrow", "down arrow", "left arrow", "right arrow"]
 
 ip = "0.0.0.0"
 port = 1234
@@ -15,8 +18,56 @@ with conn:
     while True:
         data = conn.recv(1024)
         data = data.decode("utf8")
+
         print(data)
 
-        for char in data:
-            keyboard.press(char)
-            keyboard.release(char)
+        if data.startswith("key-combination:"):
+
+            if "0" in data:
+                with keyboard.pressed(Key.alt):
+                    keyboard.press(Key.f4)
+                    keyboard.release(Key.f4)
+
+            if "1" in data:
+                print("Initiated")
+                with keyboard.pressed(Key.ctrl):
+                    with keyboard.pressed(Key.shift):
+                        keyboard.press(Key.esc)
+                        keyboard.release(Key.esc)
+
+            if "2" in data:
+                with keyboard.pressed(Key.alt):
+                    keyboard.press(Key.tab)
+                    keyboard.release(Key.tab)
+
+            if "3" in data:
+                keyboard.press(Key.cmd)
+                keyboard.release(Key.cmd)
+
+            if "4" in data:
+                keyboard.press(Key.tab)
+                keyboard.release(Key.tab)
+
+            if "5" in data:
+                keyboard.press(Key.up)
+                keyboard.release(Key.up)
+
+            if "6" in data:
+                keyboard.press(Key.down)
+                keyboard.release(Key.down)
+
+            if "7" in data:
+                keyboard.press(Key.left)
+                keyboard.release(Key.left)
+
+            if "8" in data:
+                keyboard.press(Key.right)
+                keyboard.release(Key.right)
+
+        else:
+            for part in data:
+                for char in part:
+                    keyboard.press(char)
+                    keyboard.release(char)
+                sleep(0.7)
+
