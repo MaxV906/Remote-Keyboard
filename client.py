73968e50524cb21639fcd0d57270c5cebf5a9d7e
AngeLM <leamax906@gmail.com>
2022-01-12 17:58:27 +0100

    Added escape key

--- client.py
+++ client.py
@@ -14,7 +14,7 @@ def main():
             s.send(string.encode("utf8"))
 
         elif option == "2":
-            key_combination = input("\nChoose keys:\n1)Enter\n2)Backspace\n3)cmd\n4)tab\n5)up arrow\n6)down arrow\n7)left arrow\n8) right arrow\n9)alt+f4\n10)ctrl+shift+esc\n11)alt+tab\n12)cmd+r\nYour answer: ")
+            key_combination = input("\nChoose keys:\n1)Enter\n2)Backspace\n3)esc\n4)cmd\n5)tab\n6)up arrow\n7)down arrow\n8)left arrow\n9)right arrow\n10)alt+f4\n11)ctrl+shift+esc\n12)alt+tab\n13)cmd+r\nYour answer: ")
             string = "key-combination: " + str(int(key_combination) - 1)
             s.send(string.encode("utf8"))
 
